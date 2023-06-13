05e691dae01046be0a7c313963fe8669139dd493
AmanMayank <aman.msingh87@gmail.com>
2023-06-13 23:47:04 +0530

    AddUserFunctionalityAdded

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
   //DEV ONLY
-  await pause(5000);
+  await pause(2000);
   return response.data;
 });
 
