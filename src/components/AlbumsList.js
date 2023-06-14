ba8d510aef14092431459e70d20056e0ed1d4eb8
AmanMayank <aman.msingh87@gmail.com>
2023-06-14 20:41:14 +0530

    reduxToolkitQuerySetup

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,9 @@
+import { useFetchAlbumsQuery } from "../store";
+
 function AlbumsList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  console.log(data, error, isLoading);
   return <div>Albums For {user.name}</div>;
 }
 
